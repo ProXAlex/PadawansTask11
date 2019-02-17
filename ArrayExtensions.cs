757260b6224de99bd92de02a4a1a4f110352025d
Alex <alex_goreloshev@mail.ru>
2019-02-17 14:08:53 +0300

    implement FindIndex

--- ArrayExtensions.cs
+++ ArrayExtensions.cs
@@ -6,8 +6,31 @@ namespace PadawansTask11
     {
         public static int? FindIndex(double[] array, double accuracy)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (array == null)
+                throw new ArgumentNullException();
+
+            int count = 0;
+            while (accuracy * Math.Pow(10, 1 + count) % 10 != 0)
+                count++;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                double sum1 = 0;
+                for (int j = 0; j < i; j++)
+                {
+                    sum1 += Math.Round(array[j], count);
+                }
+
+                double sum2 = 0;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    sum2 += Math.Round(array[j], count);
+                }
+                if (sum1 == sum2)
+                    return i;
+            }
+
+            return null;
         }
     }
 }

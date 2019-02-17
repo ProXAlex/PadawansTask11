45e6fe45596a7f710350b17c473c62ee50c1e2a7
Alex <alex_goreloshev@mail.ru>
2019-02-17 15:05:57 +0300

    floating poing comparison

--- ArrayExtensions.cs
+++ ArrayExtensions.cs
@@ -8,8 +8,10 @@ namespace PadawansTask11
         {
             if (array == null)
                 throw new ArgumentNullException();
-            if (accuracy < 0 || accuracy > 1)
-                throw new ArgumentException();
+            if (array.Length == 0)
+                throw new ArgumentException("The Array Cannot Be Empty.");
+            if (accuracy <= 0 || accuracy >= 1)
+                throw new ArgumentOutOfRangeException("The Accuracy Cannot Be Less Than Zero Or More Than One.");
 
             int count = BitConverter.GetBytes(decimal.GetBits((decimal)accuracy)[3])[2];
 
@@ -18,15 +20,16 @@ namespace PadawansTask11
                 double sum1 = 0;
                 for (int j = 0; j < i; j++)
                 {
-                    sum1 += Math.Round(array[j], count);
+                   sum1 += Math.Round(array[j], count);
+                 
                 }
-
                 double sum2 = 0;
                 for (int j = i + 1; j < array.Length; j++)
                 {
                     sum2 += Math.Round(array[j], count);
+                   
                 }
-                if (sum1 == sum2)
+                if (Math.Abs(sum1 - sum2) < accuracy)
                     return i;
             }
 

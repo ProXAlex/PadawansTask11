ea218db9d2e54daa61ba7e5c66f2ff08874dfe40
Alex <alex_goreloshev@mail.ru>
2019-02-17 15:06:59 +0300

    delete test

--- Tests/PublicTest.cs
+++ Tests/PublicTest.cs
@@ -6,7 +6,6 @@ namespace PadawansTask11.Tests
     public class PublicTest
     {
         [TestCase(new[] { 0.0001, 1.0002, -0.0003, 1, 0.0003, 1, -0.0003 }, 0.001, ExpectedResult = 3)]
-        [TestCase(new[] { 0.0001, 1.0002, -0.0003, 1, 0.0003, 1, -0.0003 }, 1E-08,0d, ExpectedResult = 3)]
         [TestCase(new[] { 1.0001, 1.0002, -3.0003, 1, 0.0003, 8.9, -0.0003, 0.901 }, 0.001, ExpectedResult = null)]
         public int? FindIndexTests(double[] array, double accuracy)
             => ArrayExtensions.FindIndex(array, accuracy);
